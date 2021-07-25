cbc54964f58eb51184a1709fbb8a527d44ed09b4
Shubham Saket <63151463+shubhamsaket49@users.noreply.github.com>
2021-07-25 10:31:31 +0530

    Add files via upload

---
+++ Contests/Codechef/Starters/July21/1.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		
+		int r1,w1,c1;
+		int r2,w2,c2;
+		cin>>r1>>w1>>c1;
+		cin>>r2>>w2>>c2;
+		if(r1>r2 && w1>w2 && c1>c2)
+		   cout<<"A"<<endl;
+		else if(r1<r2 && w1 < w2 && c1 <c2)
+		   cout<<"B"<<endl;
+		else if((r1>r2 && w1 >w2)||(r1>r2 && c1>c2)|| (w1> w2 && c1 >c2))
+		  cout<<"A"<<endl;
+		else
+		  cout<<"B"<<endl;
+		
+		
+	}
+	
+	
+}
